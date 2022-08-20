d488b63d3135a8b305bb0032a4d0bab7d8fb7e03
JARVIS SONG <4566733+easybest@users.noreply.github.com>
2022-08-20 17:12:43 +0800

    get started

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-data-mybatis-docs
+The documentation of Spring Data Mybatis
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+    title: 'Spring Data MyBatis',
+    description: 'Spring Data MyBatis Documentation',
+    appearance: true,
+    lastUpdated: true,
+    head: [
+        ['link', {rel: 'shortcut icon', type: 'image/png', href: '/logo.png'}]
+    ],
+    themeConfig: {
+        footer: {
+            message: 'Released under The Apache 2.0 License.',
+            copyright: 'Copyright © 2016-2023 EasyBest.IO'
+        }
+    }
+}
